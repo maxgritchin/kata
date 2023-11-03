094f378bba55e8ed4be1f7c98c06987657b10ef3
gritmax <max.gritchin@gmail.com>
2023-11-03 10:54:53 +0700

    Add Haversine calculator template

---
+++ Cteleport.Airport/Airport.Measure.Implementation/Services/DistanceCalculators/HaversineFormula.cs
@@ -0,0 +1,17 @@
+using Airport.Measure.Domain.Entities;
+using Airport.Measure.Domain.Services;
+
+namespace Airport.Measure.Implementation.Services.DistanceCalculators;
+
+/// <summary>
+/// Haversine formula to calculate distance between two points on a sphere
+/// <remarks>https://en.wikipedia.org/wiki/Haversine_formula</remarks>
+/// </summary>
+public class HaversineFormula: IDistanceCalculator
+{
+    /// <inheritdoc />
+    public Distance Calculate(LocationPoint form, LocationPoint to)
+    {
+        throw new NotImplementedException();
+    }
+}
